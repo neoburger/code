356d2f417c0ae5c9989a20dc107e97a23b553623
Hecate2 <2474101468@qq.com>
2021-11-09 16:47:20 +0800

    class NeoBurgerGovernanceToken; changeable TEE

--- NeoBurgerGovernanceToken.cs
+++ NeoBurgerGovernanceToken.cs
@@ -13,16 +13,26 @@ namespace NeoBurger
     [ManifestExtra("Description", "NeoBurger Governance Token")]
     [SupportedStandards("NEP-17")]
     [ContractPermission("*", "*")]
-    public class NeoBurgerGovernanceTokenMainnet : Nep17Token
+    public class NeoBurgerGovernanceToken : Nep17Token
     {
         [InitialValue("[TODO]: ARGS", ContractParameterType.Hash160)]
-        private const UInt160 OWNER = default;
+        private const byte PREFIX_TEE = 0x50;
         public override byte Decimals() => 8;
         public override string Symbol() => "NOBUG";
+
+        public static UInt160 GetTEE() => (UInt160)Storage.Get(Storage.CurrentContext, new byte[] { PREFIX_TEE });
+
         public static object ExecuteProposal(UInt160 scripthash, string method, ByteString[] args)
         {
-            Runtime.CheckWitness(OWNER);
+            Runtime.CheckWitness((UInt160)Storage.Get(Storage.CurrentContext, new byte[] { PREFIX_TEE }));
             return Contract.Call(scripthash, method, CallFlags.All, args);
+        }
+
+        public static void ChangeTEE(UInt160 newTEE)
+        {
+            ByteString new_tee_bytearray = (ByteString)new byte[] { PREFIX_TEE };
+            Runtime.CheckWitness((UInt160)Storage.Get(Storage.CurrentContext, new_tee_bytearray));
+            Storage.Put(Storage.CurrentContext, new_tee_bytearray, newTEE);
         }
 
         public static void OnNEP17Payment(UInt160 from, BigInteger amount, object data)
@@ -31,7 +41,7 @@ namespace NeoBurger
 
         public static void Update(ByteString nefFile, string manifest)
         {
-            ExecutionEngine.Assert(Runtime.CheckWitness(OWNER));
+            ExecutionEngine.Assert(Runtime.CheckWitness(Runtime.ExecutingScriptHash));
             ContractManagement.Update(nefFile, manifest, null);
         }
     }
